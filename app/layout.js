bb47190ffc9e213538b5df171b1889c48be06a2a
VionexusInt <vionexusinteractive@gmail.com>
2026-07-13 00:45:39 +0200

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -19,7 +19,7 @@ export const viewport = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="es" suppressHydrationWarning>
+    <html lang="es">
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
